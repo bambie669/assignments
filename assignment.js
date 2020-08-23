931a9fe719f45c82b3867508fb711420dbde050b
bambie669 <raduroman94@gmail.com>
2020-08-23 10:17:49 -0700

    initial  commit

---
+++ assignment.js
@@ -0,0 +1,20 @@
+// Create two variables: One that holds a fictional user input (a number of
+// your choice) and one that holds no value initially (e.g. "result").
+
+const userInput = 10;
+let result;
+
+// Set the result variable to 18 plus the value stored in user input.
+
+result = 18 + userInput;
+
+// Add three additional lines of code where you change the result variable
+// again (by subtracting a value of your choice, multiplying it and
+// dividing it).
+
+result = result + 10;
+result = result / 2;
+result = result * 9;
+
+alert(result);
+alert(userInput);
